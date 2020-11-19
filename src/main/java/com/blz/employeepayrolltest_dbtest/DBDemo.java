9c001e45f996b54ac8ad32d2d6fb062640feca25
archanabotla08 <archana.botla@gmail.com>
2020-11-19 20:15:12 +0530

    [Archana] Added: UC1 to create connetion with database

--- src/main/java/com/blz/employeepayrolltest_dbtest/DBDemo.java
+++ src/main/java/com/blz/employeepayrolltest_dbtest/DBDemo.java
@@ -1,10 +1,40 @@
 package com.blz.employeepayrolltest_dbtest;
 
 import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
 
 public class DBDemo {
 	public static void main(String[] args) {
-		System.out.println("Welcome Message");
+		// System.out.println("Welcome Message");
+		String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll?useSSL=false";
+		String userName = "root";
+		String password = "root";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver Loaded");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in class path !!");
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to Database: " + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection Successful !!!" + connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
+	public static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println(" " + driverClass.getClass().getName());
+		}
+
+	}
+
 }
