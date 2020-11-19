027b6c53144ae22e1726eeeff78ac852b5e6e3fe
archanabotla08 <archana.botla@gmail.com>
2020-11-19 17:57:40 +0530

    [Archana] Added: Welcome message to the master

---
+++ src/main/java/com/blz/employeepayrolltest_dbtest/DBDemo.java
@@ -0,0 +1,10 @@
+package com.blz.employeepayrolltest_dbtest;
+
+import java.sql.Connection;
+
+public class DBDemo {
+	public static void main(String[] args) {
+		System.out.println("Welcome Message");
+	}
+	
+}
